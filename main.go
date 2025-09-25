14a1a6b9c9b5b732deff3f960ffdaa13365e081f
Peter Lin <petlin@google.com>
2025-09-24 20:07:16 -0400

    Update main.go

--- main.go
+++ main.go
@@ -1,24 +1,12 @@
 package main
 
-import (
-	"fmt"
-	"math/rand"
-)
+import "fmt"
 
 func main() {
-	// number between 1 and 3
-	randomNumber := rand.Intn(3) + 1
-	item := GoogleRagSimpsonsSpiderPig(randomNumber)
-	fmt.Println(item)
+	fmt.Println(SpiderPig())
 }
 
-// GoogleRagSimpsonsSpiderPig returns a vegetable based on the number provided
-func GoogleRagSimpsonsSpiderPig(i int) string {
-	if i == 0 {
-		return "brocolli"
-	} else if i == 1 {
-		return "spinach"
-	} else {
-		return "cabbage"
-	}
+// SpiderPig returns a string
+func SpiderPig() string {
+	return "web"
 }
